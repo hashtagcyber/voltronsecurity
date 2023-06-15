d73e44b7f4634cf4d80a9f6cb818da86287f25dc
Matt Domko <hashtagcyber@users.noreply.github.com>
2023-06-15 11:02:32 -0500

    Added tests and AzureServiceBus read/write

---
+++ src/voltronsecurity/voltron_azure.py
@@ -0,0 +1,124 @@
+from voltron_base import (
+    VoltronBaseProcessResponse,
+    VoltronMessagePayload,
+    VoltronBaseMessageInterface,
+)
+import logging
+import json
+import asyncio
+import os
+import time
+
+from typing import Optional
+
+from azure.servicebus.aio import ServiceBusClient
+from azure.servicebus import ServiceBusMessage
+from azure.identity.aio import DefaultAzureCredential
+
+FORMAT = "[%(filename)s:%(lineno)s - %(funcName)s() ] %(message)s"
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger("voltron")
+
+
+class VoltronAzureServiceBusQueue(VoltronBaseMessageInterface):
+    def __init__(
+        self, queue_name: str, namespace: str, credential: DefaultAzureCredential
+    ):
+        self.queue_name = queue_name
+        self.namespace = namespace
+        self.creds = credential
+
+    def get_client(
+        self, namespace: Optional[str] = None, creds: Optional[str] = None
+    ) -> ServiceBusClient:
+        if namespace is None:
+            namespace = self.namespace
+        if creds is None:
+            creds = self.creds
+        client = ServiceBusClient(namespace, creds)
+        return client
+
+    async def handle_messages(
+        self, client: Optional[ServiceBusClient] = None, queue: Optional[str] = None
+    ) -> list[VoltronBaseProcessResponse]:
+        """Primary async loop for retrieving and processing messages.
+        Awaits other class methods to perform an action on each message.
+        """
+        if queue is None:
+            queue = self.queue_name
+        if client is None:
+            client = self.get_client()
+        results = []
+        async with client:
+            receiver = client.get_queue_receiver(queue_name=queue)
+            async with receiver:
+                received = await receiver.receive_messages(
+                    max_wait_time=5, max_message_count=1
+                )
+                for msg in received:
+                    logger.debug(str(msg))
+                    resp = await self.process_message(json.loads(str(msg)))
+                    results.append(resp)
+                    if resp["success"]:
+                        await receiver.complete_message(msg)
+                    else:
+                        continue
+        await self.creds.close()
+        return results
+
+    async def process_message(
+        self, message: VoltronMessagePayload
+    ) -> VoltronBaseProcessResponse:
+        """Override this in child class to perform an action based on the message received."""
+        logger.debug("Processed Message : {}".format(message))
+        response = {"success": True, "message": "Default Message Processor. No action taken. Message deleted.", "data": message}
+        return response
+
+    def generate_message(self, handlerName:str, handlerConfig:dict, handlerData:dict, messageSource:str, startTime:int ) -> VoltronMessagePayload:
+        body = {'handlerName':handlerName, 'handlerConfig': handlerConfig, 'handlerData':handlerData, 'messageSource':messageSource, 'startTime': startTime}
+        message = ServiceBusMessage(body=json.dumps(body), content_type='application/json')
+        return message
+
+    async def send_message(
+        self, message: VoltronMessagePayload, client: Optional[ServiceBusClient] = None, queue: Optional[str] = None
+    ) -> VoltronBaseProcessResponse:
+        if queue is None:
+            queue = self.queue_name
+        if client is None:
+            client = self.get_client()
+        servicebus_message = self.generate_message(message['handlerName'],message['handlerconfig'],message['handlerData'],message['messageSource'],message['startTime'])
+        message_list = [servicebus_message]
+        async with client:
+            sender = client.get_queue_sender(queue_name=queue)
+            async with sender:
+                try:
+                    await sender.send_messages(message_list)
+                    response = {'success':True, 'message':'Sent {} messages'.format(len(message_list))}
+                except Exception as e:
+                    response = {'success':False, 'message':str(e)}
+                    
+        return response
+
+if __name__ == "__main__":
+    logger.setLevel('DEBUG')
+    logger.debug('Getting Environs')
+    try:
+        VOLTRON_ASB_QUEUE = os.environ['VOLTRON_QUEUE']
+        VOLTRON_ASB_NAMESPACE = os.environ['VOLTRON_ASB_NAMESPACE']
+    except KeyError as e:
+        logger.error('Required environment variable {} not set'.format(e))
+        raise SystemExit(1)
+    logger.debug('Setting Creds')
+    creds = DefaultAzureCredential()
+    logger.debug('Creating Queue Handler')
+    voltbus = VoltronAzureServiceBusQueue(
+        VOLTRON_ASB_QUEUE, VOLTRON_ASB_NAMESPACE, creds
+    )
+    sample_message = {'handlerName':'samplehandler','handlerconfig':{},'handlerData':{},'messageSource':'voltron_azure.py','startTime':int(time.time())}
+    logger.debug('Sample Message:\n\t{}'.format(sample_message))
+    logger.debug('Writing message to queue')
+    send_results = asyncio.run(voltbus.send_message(sample_message))
+    logger.debug(send_results)
+    logger.debug('Getting messages from queue')
+    results = asyncio.run(voltbus.handle_messages())
+    logger.debug(results)
---
+++ tests/test_voltron_azure.py
@@ -0,0 +1,116 @@
+import asyncio
+import datetime
+import unittest
+import json
+from unittest import mock
+from src.voltronsecurity.voltron_azure import (
+    VoltronAzureServiceBusQueue,
+    DefaultAzureCredential,
+    ServiceBusClient,
+    ServiceBusMessage
+)
+from azure.servicebus.aio import ServiceBusReceiver, ServiceBusSender
+
+
+class TestVoltronAzureServiceBusQueue(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sample_queue_name = "mytestqueue"
+        self.sample_namespace = "mytestnamespace"
+        self.sample_creds = mock.MagicMock(spec=DefaultAzureCredential)
+        self.sample_voltron_payload = {'handlerName':'samplehandler','handlerconfig':{},'handlerData':{},'messageSource':'test_voltron_azure.py','startTime':12345}
+        self.sample_received_messages = [ServiceBusMessage(json.dumps(self.sample_voltron_payload))]
+
+    @mock.patch("src.voltronsecurity.voltron_azure.ServiceBusClient", return_value=mock.AsyncMock())
+    def test_init(self, mock_sbc):
+        handler = VoltronAzureServiceBusQueue(
+            self.sample_queue_name, self.sample_namespace, self.sample_creds
+        )
+        self.assertEqual(self.sample_queue_name, handler.queue_name)
+        self.assertEqual(self.sample_namespace, handler.namespace)
+        self.assertEqual(type(self.sample_creds), type(handler.creds))
+
+    @mock.patch("src.voltronsecurity.voltron_azure.ServiceBusClient", return_value=mock.AsyncMock())
+    def test_happy_default_get_client(self, mock_sbc):
+        handler = VoltronAzureServiceBusQueue(
+            self.sample_queue_name, self.sample_namespace, self.sample_creds
+        )
+        client = handler.get_client()
+        mock_sbc.assert_called_with(self.sample_namespace, self.sample_creds)
+
+
+    @mock.patch("src.voltronsecurity.voltron_azure.ServiceBusClient")
+    def test_happy_default_handle_messages(self, mock_sbc):
+        mock_sbr = mock.AsyncMock(spec=ServiceBusReceiver)
+        mock_sbr.return_value.__aenter__.return_value = mock_sbr.return_value
+        mock_sbr.return_value.__aexit__.return_value = None
+        mock_sbr.receive_messages.return_value = self.sample_received_messages
+
+        mock_sbc.return_value.get_queue_receiver.return_value = mock_sbr
+        handler = VoltronAzureServiceBusQueue(self.sample_queue_name, self.sample_namespace, self.sample_creds)
+        resp = asyncio.run(handler.handle_messages())
+        mock_sbr.complete_message.assert_called()
+    
+    @mock.patch("src.voltronsecurity.voltron_azure.VoltronAzureServiceBusQueue.process_message", return_value={"success": False, "message": "MockedFail", "data": {}})
+    @mock.patch("src.voltronsecurity.voltron_azure.ServiceBusClient")
+    def test_failed_process_messages(self, mock_sbc, mock_vsb):
+        mock_sbr = mock.AsyncMock(spec=ServiceBusReceiver)
+        mock_sbr.return_value.__aenter__.return_value = mock_sbr.return_value
+        mock_sbr.return_value.__aexit__.return_value = None
+        mock_sbr.receive_messages.return_value = self.sample_received_messages
+
+        mock_sbc.return_value.get_queue_receiver.return_value = mock_sbr
+        handler = VoltronAzureServiceBusQueue(self.sample_queue_name, self.sample_namespace, self.sample_creds)
+        resp = asyncio.run(handler.handle_messages())
+        mock_sbr.complete_message.assert_not_called()
+        self.assertIn('success', resp[0])
+        self.assertIn('message', resp[0])
+        self.assertIn('data', resp[0])
+        self.assertEqual(resp[0]["success"], False)
+    
+    def test_generate_message(self):
+        handler = VoltronAzureServiceBusQueue(self.sample_queue_name, self.sample_namespace, self.sample_creds)
+        msg_h_name = 'samplehandlerName'
+        msg_h_config = {}
+        msg_h_data = {}
+        msg_source = 'samplemessageSource'
+        msg_start_time = 1234
+
+        new_message = handler.generate_message(msg_h_name, msg_h_config, msg_h_data, msg_source, msg_start_time)
+        msg_json = json.loads(str(new_message))
+
+        self.assertEqual(type(new_message), ServiceBusMessage)
+        self.assertEqual(msg_json['handlerName'],msg_h_name)
+        self.assertEqual(msg_json['handlerConfig'],msg_h_config)
+        self.assertEqual(msg_json['handlerData'],msg_h_data)
+        self.assertEqual(msg_json['messageSource'],msg_source)
+        self.assertEqual(msg_json['startTime'],msg_start_time)
+    
+    @mock.patch("src.voltronsecurity.voltron_azure.ServiceBusClient")
+    def test_happy_default_send_message(self, mock_sbc):
+        mock_sbs = mock.AsyncMock(spec=ServiceBusSender)
+        mock_sbs.return_value.__aenter__.return_value = mock_sbs.return_value
+        mock_sbs.return_value.__aexit__.return_value = None
+        mock_sbs.send_messages.return_value = None
+
+        mock_sbc.return_value.get_queue_sender.return_value = mock_sbs
+        handler = VoltronAzureServiceBusQueue(self.sample_queue_name, self.sample_namespace, self.sample_creds)
+        resp = asyncio.run(handler.send_message(self.sample_voltron_payload))
+        self.assertIn('success', resp)
+        self.assertIn('message', resp)
+        self.assertEqual(resp['success'], True)
+
+    @mock.patch("src.voltronsecurity.voltron_azure.ServiceBusClient")
+    def test_failed_default_send_message(self, mock_sbc):
+        mock_sbs = mock.AsyncMock(spec=ServiceBusSender)
+        mock_sbs.return_value.__aenter__.return_value = mock_sbs.return_value
+        mock_sbs.return_value.__aexit__.return_value = None
+        mock_sbs.send_messages.side_effect = Exception('TestSendFailure')
+
+        mock_sbc.return_value.get_queue_sender.return_value = mock_sbs
+        handler = VoltronAzureServiceBusQueue(self.sample_queue_name, self.sample_namespace, self.sample_creds)
+        resp = asyncio.run(handler.send_message(self.sample_voltron_payload))
+        self.assertIn('success', resp)
+        self.assertIn('message', resp)
+        self.assertEqual(resp['success'], False)
+        self.assertEqual(resp['message'], 'TestSendFailure')
+        
